b8cac62e4e2f59fd54203f0f1006a585a62ef482
qixi zhang <zqx0803@me.com>
2021-09-06 17:39:38 +0800

    create component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Expenses from "./components/Expenses";
+
+
+function App() {
+  const expenses = [
+    {id: 'e1', title: 'Bread', amount: 46, date: new Date(2021, 8, 2)},
+    {id: 'e2', title: 'Coffee', amount: 28, date: new Date(2021, 8, 2)},
+    {id: 'e3', title: 'Icecream', amount: 40, date: new Date(2021, 8, 2)},
+    {id: 'e4', title: 'Eggs', amount: 8, date: new Date(2021, 8, 2)},
+  ]
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses expenses = {expenses}></Expenses>
+
+    </div>
+  );
+}
+
+export default App;
